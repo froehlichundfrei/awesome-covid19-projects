32d175db625338809d17607f075ce854427447fd
Hannes Tiede <hannes@froehlichundfrei.de>
2020-03-21 20:05:34 +0100

    feat: add plz search

--- packages/website/src/components/layout.js
+++ packages/website/src/components/layout.js
@@ -3,7 +3,7 @@
 import { jsx } from 'theme-ui';
 import React from 'react';
 import { Link, useStaticQuery, graphql, navigate } from 'gatsby';
-import { FaSearch } from 'react-icons/fa';
+import { FaMapMarker, FaSearch } from 'react-icons/fa';
 import { useCombobox } from 'downshift';
 import Fuse from 'fuse.js';
 
@@ -11,12 +11,217 @@ import * as icons from '../utils/icons';
 import { rhythm } from '../utils/typography';
 import Logo from './logo';
 
-function SearchInput(props) {
-  const [text, setText] = React.useState('');
-  const [focused, setFocused] = React.useState(false);
+// function SearchInput(props) {
+//   const [text, setText] = React.useState('');
+//   const [focused, setFocused] = React.useState(false);
+
+//   const data = useStaticQuery(graphql`
+//     query LayoutQuery {
+//       site {
+//         siteMetadata {
+//           title
+//           texts {
+//             searchPlaceholderText
+//           }
+//         }
+//       }
+//       articles: allMarkdownRemark {
+//         nodes {
+//           id
+//           fields {
+//             slug
+//             collection {
+//               icon
+//             }
+//           }
+//           frontmatter {
+//             title
+//             description
+//           }
+//           headings {
+//             # depth
+//             value
+//           }
+//           # excerpt(format: PLAIN)
+//         }
+//       }
+//     }
+//   `);
+
+//   const items = data.articles.nodes;
+
+//   const fuse = React.useMemo(
+//     () =>
+//       new Fuse(items, {
+//         shouldSort: true,
+//         tokenize: true,
+//         threshold: 0.6,
+//         location: 0,
+//         distance: 100,
+//         maxPatternLength: 32,
+//         minMatchCharLength: 1,
+//         keys: [
+//           'frontmatter.title',
+//           'frontmatter.description',
+//           'headings.value',
+//         ],
+//       }),
+//     [items],
+//   );
+
+//   const [inputItems, setInputItems] = React.useState(data.articles.nodes);
+
+//   const combobox = useCombobox({
+//     items: inputItems,
+//     onInputValueChange: ({ inputValue }) => {
+//       setInputItems(fuse.search(inputValue));
+//     },
+//     itemToString: node => (node ? node.frontmatter.title : ''),
+//     onSelectedItemChange: ({ selectedItem }) => {
+//       navigate(selectedItem.fields.slug);
+//     },
+//   });
+
+//   return (
+//     <div sx={{ position: 'relative' }} {...combobox.getComboboxProps()}>
+//       <label
+//         htmlFor="search"
+//         {...combobox.getLabelProps({
+//           htmlFor: 'search',
+//         })}
+//         sx={{
+//           position: 'absolute',
+//           left: '18pt',
+//           top: '0',
+//           bottom: '0',
+//           display: 'flex',
+//           alignItems: 'center',
+//           cursor: 'text',
+//         }}
+//       >
+//         <FaSearch color={focused ? '#828A97' : 'rgba(255,255,255,0.9)'} />
+//       </label>
+//       <input
+//         id="search"
+//         type="text"
+//         value={text}
+//         autoFocus
+//         onChange={event => setText(event.target.value)}
+//         placeholder={data.site.siteMetadata.texts.searchPlaceholderText}
+//         autoComplete="off"
+//         sx={{
+//           backgroundColor: 'rgba(255,255,255,0.2)',
+//           transition: 'background .4s, box-shadow .2s',
+//           width: '100%',
+//           padding: '20px 32px 21px 56px',
+//           background: 'rgba(255,255,255,0.1)',
+//           border: 'none',
+//           outline: 'none',
+//           color: 'searchTextColor',
+//           fontSize: '18px',
+//           lineHeight: '18px',
+//           borderRadius: 2,
+//           '&:focus': {
+//             backgroundColor: 'white',
+//             boxShadow: '0 10px 20px rgba(0,0,0,0.14)',
+//             color: 'searchTextFocusColor',
+//           },
+//           '::placeholder': {
+//             color: 'searchTextPlaceholderColor',
+//           },
+//           '&:focus::placeholder': {
+//             color: 'searchTextFocusPlaceholderColor',
+//           },
+//         }}
+//         {...combobox.getInputProps({
+//           id: 'search',
+//           onFocus: () => setFocused(true),
+//           onBlur: () => setFocused(false),
+//         })}
+//       />
+//       <div
+//         {...combobox.getMenuProps()}
+//         sx={{
+//           position: 'absolute',
+//           left: 0,
+//           right: 0,
+//           top: 'calc(20px + 21px + 18px)',
+//           alignItems: 'center',
+//           cursor: 'text',
+//           background: 'white',
+//           color: 'comboboxColor',
+//           zIndex: 4,
+//           borderBottomLeftRadius: 2,
+//           borderBottomRightRadius: 2,
+//           boxShadow: '0 3px 8px 0 rgba(0,0,0,0.03)',
+//         }}
+//       >
+//         {combobox.isOpen &&
+//           inputItems.map((node, index) => {
+//             // skip drafts and "hidden" articles (ones without a collection)
+//             if (!node.fields.collection) return null;
+
+//             const icon = jsx(
+//               icons[node.fields.collection.icon],
+//               { sx: { color: 'iconColor' }, size: '2rem' },
+//               null,
+//             );
+//             return (
+//               <Link
+//                 key={node.id}
+//                 to={node.fields.slug}
+//                 sx={{
+//                   display: 'flex',
+//                   pl: 3,
+//                   pr: 5,
+//                   py: 3,
+//                   textDecoration: 'none',
+//                   background:
+//                     combobox.highlightedIndex === index ? '#E5E5E5' : 'initial',
+//                   '&:hover': {
+//                     textDecoration: 'none',
+//                   },
+//                 }}
+//                 {...combobox.getItemProps({ item: node, index })}
+//               >
+//                 <div
+//                   sx={{
+//                     display: ['none', 'flex'],
+//                     alignItems: 'center',
+//                     pr: 3,
+//                   }}
+//                 >
+//                   {icon}
+//                 </div>
+//                 <div sx={{ flex: 'auto' }}>
+//                   <h3 sx={{ my: 0, fontSize: 3 }}>{node.frontmatter.title}</h3>
+//                   <p
+//                     sx={{
+//                       my: 0,
+//                       color: 'articleDescriptionColor',
+//                       fontSize: [1, 2],
+//                     }}
+//                   >
+//                     {node.frontmatter.description}
+//                   </p>
+//                 </div>
+//               </Link>
+//             );
+//           })}
+//       </div>
+//     </div>
+//   );
+// }
+
+function SearchInputNew(props) {
+  const [areaCodeText, setAreaCodeText] = React.useState('');
+  const [areaCodeFocused, setAreaCodeFocused] = React.useState(false);
+
+  const [searchText, setSearchText] = React.useState('');
+  const [searchFocused, setSearchFocused] = React.useState(false);
 
   const data = useStaticQuery(graphql`
-    query LayoutQuery {
+    query LayoutQueryNew {
       site {
         siteMetadata {
           title
@@ -71,143 +276,117 @@ function SearchInput(props) {
 
   const [inputItems, setInputItems] = React.useState(data.articles.nodes);
 
-  const combobox = useCombobox({
-    items: inputItems,
-    onInputValueChange: ({ inputValue }) => {
-      setInputItems(fuse.search(inputValue));
-    },
-    itemToString: node => (node ? node.frontmatter.title : ''),
-    onSelectedItemChange: ({ selectedItem }) => {
-      navigate(selectedItem.fields.slug);
-    },
-  });
-
   return (
-    <div sx={{ position: 'relative' }} {...combobox.getComboboxProps()}>
-      <label
-        htmlFor="search"
-        {...combobox.getLabelProps({
-          htmlFor: 'search',
-        })}
-        sx={{
-          position: 'absolute',
-          left: '18pt',
-          top: '0',
-          bottom: '0',
-          display: 'flex',
-          alignItems: 'center',
-          cursor: 'text',
-        }}
-      >
-        <FaSearch color={focused ? '#828A97' : 'rgba(255,255,255,0.9)'} />
-      </label>
-      <input
-        id="search"
-        type="text"
-        value={text}
-        autoFocus
-        onChange={event => setText(event.target.value)}
-        placeholder={data.site.siteMetadata.texts.searchPlaceholderText}
-        autoComplete="off"
-        sx={{
-          backgroundColor: 'rgba(255,255,255,0.2)',
-          transition: 'background .4s, box-shadow .2s',
-          width: '100%',
-          padding: '20px 32px 21px 56px',
-          background: 'rgba(255,255,255,0.1)',
-          border: 'none',
-          outline: 'none',
-          color: 'searchTextColor',
-          fontSize: '18px',
-          lineHeight: '18px',
-          borderRadius: 2,
-          '&:focus': {
-            backgroundColor: 'white',
-            boxShadow: '0 10px 20px rgba(0,0,0,0.14)',
-            color: 'searchTextFocusColor',
-          },
-          '::placeholder': {
-            color: 'searchTextPlaceholderColor',
-          },
-          '&:focus::placeholder': {
-            color: 'searchTextFocusPlaceholderColor',
-          },
-        }}
-        {...combobox.getInputProps({
-          id: 'search',
-          onFocus: () => setFocused(true),
-          onBlur: () => setFocused(false),
-        })}
-      />
-      <div
-        {...combobox.getMenuProps()}
-        sx={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 'calc(20px + 21px + 18px)',
-          alignItems: 'center',
-          cursor: 'text',
-          background: 'white',
-          color: 'comboboxColor',
-          zIndex: 4,
-          borderBottomLeftRadius: 2,
-          borderBottomRightRadius: 2,
-          boxShadow: '0 3px 8px 0 rgba(0,0,0,0.03)',
-        }}
-      >
-        {combobox.isOpen &&
-          inputItems.map((node, index) => {
-            // skip drafts and "hidden" articles (ones without a collection)
-            if (!node.fields.collection) return null;
-
-            const icon = jsx(
-              icons[node.fields.collection.icon],
-              { sx: { color: 'iconColor' }, size: '2rem' },
-              null,
-            );
-            return (
-              <Link
-                key={node.id}
-                to={node.fields.slug}
-                sx={{
-                  display: 'flex',
-                  pl: 3,
-                  pr: 5,
-                  py: 3,
-                  textDecoration: 'none',
-                  background:
-                    combobox.highlightedIndex === index ? '#E5E5E5' : 'initial',
-                  '&:hover': {
-                    textDecoration: 'none',
-                  },
-                }}
-                {...combobox.getItemProps({ item: node, index })}
-              >
-                <div
-                  sx={{
-                    display: ['none', 'flex'],
-                    alignItems: 'center',
-                    pr: 3,
-                  }}
-                >
-                  {icon}
-                </div>
-                <div sx={{ flex: 'auto' }}>
-                  <h3 sx={{ my: 0, fontSize: 3 }}>{node.frontmatter.title}</h3>
-                  <p
-                    sx={{
-                      my: 0,
-                      color: 'articleDescriptionColor',
-                      fontSize: [1, 2],
-                    }}
-                  >
-                    {node.frontmatter.description}
-                  </p>
-                </div>
-              </Link>
-            );
-          })}
+    <div
+      sx={{
+        display: 'flex',
+        padding: '30px',
+      }}
+    >
+      <div sx={{ position: 'relative', flex: '30%' }}>
+        <label
+          htmlFor="areaCode"
+          sx={{
+            position: 'absolute',
+            left: '18pt',
+            top: '0',
+            bottom: '0',
+            display: 'flex',
+            alignItems: 'center',
+            cursor: 'text',
+          }}
+        >
+          <FaMapMarker
+            color={areaCodeFocused ? '#828A97' : 'rgba(255,255,255,0.9)'}
+          />
+        </label>
+        <input
+          id="areaCode"
+          type="text"
+          value={areaCodeText}
+          autoFocus
+          onChange={event =>
+            /^[0-9]{0,5}$/.test(event.target.value) &&
+            setAreaCodeText(event.target.value)
+          }
+          placeholder="PLZ"
+          autoComplete="off"
+          sx={{
+            backgroundColor: 'rgba(255,255,255,0.2)',
+            transition: 'background .4s, box-shadow .2s',
+            width: '100%',
+            padding: '20px 32px 21px 56px',
+            background: 'rgba(255,255,255,0.1)',
+            border: 'none',
+            outline: 'none',
+            color: 'searchTextColor',
+            fontSize: '18px',
+            lineHeight: '18px',
+            borderRadius: 2,
+            '&:focus': {
+              backgroundColor: 'white',
+              boxShadow: '0 10px 20px rgba(0,0,0,0.14)',
+              color: 'searchTextFocusColor',
+            },
+            '::placeholder': {
+              color: 'searchTextPlaceholderColor',
+            },
+            '&:focus::placeholder': {
+              color: 'searchTextFocusPlaceholderColor',
+            },
+          }}
+        />
+      </div>
+      <div sx={{ position: 'relative', flex: '60%', marginLeft: '20px' }}>
+        <label
+          htmlFor="search"
+          sx={{
+            position: 'absolute',
+            left: '18pt',
+            top: '0',
+            bottom: '0',
+            display: 'flex',
+            alignItems: 'center',
+            cursor: 'text',
+          }}
+        >
+          <FaSearch
+            color={searchFocused ? '#828A97' : 'rgba(255,255,255,0.9)'}
+          />
+        </label>
+        <input
+          id="search"
+          type="text"
+          value={searchText}
+          // autoFocus
+          onChange={event => setSearchText(event.target.value)}
+          placeholder={data.site.siteMetadata.texts.searchPlaceholderText}
+          autoComplete="off"
+          sx={{
+            backgroundColor: 'rgba(255,255,255,0.2)',
+            transition: 'background .4s, box-shadow .2s',
+            width: '100%',
+            padding: '20px 32px 21px 56px',
+            background: 'rgba(255,255,255,0.1)',
+            border: 'none',
+            outline: 'none',
+            color: 'searchTextColor',
+            fontSize: '18px',
+            lineHeight: '18px',
+            borderRadius: 2,
+            '&:focus': {
+              backgroundColor: 'white',
+              boxShadow: '0 10px 20px rgba(0,0,0,0.14)',
+              color: 'searchTextFocusColor',
+            },
+            '::placeholder': {
+              color: 'searchTextPlaceholderColor',
+            },
+            '&:focus::placeholder': {
+              color: 'searchTextFocusPlaceholderColor',
+            },
+          }}
+        />
       </div>
     </div>
   );
@@ -238,11 +417,11 @@ class Layout extends React.Component {
                 pb: 2,
               }}
             >
-              <Logo color="white" size={['36px', '48px']} />
+              {/* <Logo color="white" size={['36px', '48px']} />
               <p sx={{ pt: 2, pb: 2, mb: 2, mt: 2, fontSize: [2, 3] }}>
                 {description}
-              </p>
-              <SearchInput />
+              </p> */}
+              <SearchInputNew />
             </header>
           ) : (
             <header
@@ -255,7 +434,7 @@ class Layout extends React.Component {
                 pb: 2,
               }}
             >
-              <h3
+              {/* <h3
                 sx={{
                   mt: 0,
                   mb: 3,
@@ -276,8 +455,8 @@ class Layout extends React.Component {
                   <Logo color="white" size={['36px', '48px']} />
                 </Link>
               </h3>
-              {location.pathname === rootPath && <p>{description}</p>}
-              <SearchInput />
+              {location.pathname === rootPath && <p>{description}</p>} */}
+              <SearchInputNew />
             </header>
           )}
         </div>
@@ -289,7 +468,7 @@ class Layout extends React.Component {
           <main
             sx={{
               mx: `auto`,
-              maxWidth: rhythm(30),
+              maxWidth: rhythm(40),
               px: [2, 4],
               py: [3],
             }}
@@ -315,7 +494,7 @@ class Layout extends React.Component {
             {/*
               PLEASE DO NOT REMOVE THIS LINK.
 
-              A lot of unpaid time is spent on making and maintaining the 
+              A lot of unpaid time is spent on making and maintaining the
               center. Nothing is expected in return. Keeping this link here
               is the only small thing asked in return. So please don't remove it.
 
